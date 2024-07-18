4b92b062fcfb892997f60218a82295ea6169b7f9
misu13 <djmisu13@gmail.com>
2024-07-18 16:58:42 +0800

    fix:修正產品詳細資訊頁重複程式碼轉為元件

--- src/components/ProductsDetail.vue
+++ src/components/ProductsDetail.vue
@@ -3,211 +3,142 @@ import SwiperSlider from '../components/SwiperSlider.vue'
 </script>
 
 <template>
-  <div class="container">
-    <nav class="navbar navbar-expand-lg navbar-light px-0">
-      <a class="navbar-brand" href="./index.html">Navbar</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div
-        class="collapse navbar-collapse justify-content-end"
-        id="navbarNavAltMarkup"
-      >
-        <div class="navbar-nav">
-          <a class="nav-item nav-link me-4 active" href="./index.html"
-            >Home <span class="sr-only">(current)</span></a
+  <BaseLayout
+    :show-navbar="true"
+    :show-footer="true"
+    container-class="my-custom-container"
+  >
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-7">
+          <div
+            id="carouselExampleControls"
+            class="carousel slide"
+            data-ride="carousel"
           >
-          <a class="nav-item nav-link me-4" href="./product.html">Product</a>
-          <a class="nav-item nav-link me-4" href="./detail.html">Detail</a>
-          <a class="nav-item nav-link" href="./cart.html"
-            ><font-awesome-icon :icon="['fas', 'shopping-cart']"
-          /></a>
-        </div>
-      </div>
-    </nav>
-    <div class="row align-items-center">
-      <div class="col-md-7">
-        <div
-          id="carouselExampleControls"
-          class="carousel slide"
-          data-ride="carousel"
-        >
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img
-                src="https://images.unsplash.com/photo-1502743780242-f10d2ce370f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1916&q=80"
-                class="d-block w-100"
-                alt="..."
-              />
-            </div>
-            <div class="carousel-item">
-              <img
-                src="https://images.unsplash.com/photo-1502743780242-f10d2ce370f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1916&q=80"
-                class="d-block w-100"
-                alt="..."
-              />
-            </div>
-            <div class="carousel-item">
-              <img
-                src="https://images.unsplash.com/photo-1502743780242-f10d2ce370f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1916&q=80"
-                class="d-block w-100"
-                alt="..."
-              />
-            </div>
-          </div>
-          <a
-            class="carousel-control-prev"
-            href="#carouselExampleControls"
-            role="button"
-            data-slide="prev"
-          >
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a
-            class="carousel-control-next"
-            href="#carouselExampleControls"
-            role="button"
-            data-slide="next"
-          >
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
-        </div>
-      </div>
-      <div class="col-md-5">
-        <nav aria-label="breadcrumb">
-          <ol class="breadcrumb bg-white px-0 mb-0 py-3">
-            <li class="breadcrumb-item">
-              <a class="text-muted" href="./index.html">Home</a>
-            </li>
-            <li class="breadcrumb-item">
-              <a class="text-muted" href="./product.html">Product</a>
-            </li>
-            <li class="breadcrumb-item active" aria-current="page">Detail</li>
-          </ol>
-        </nav>
-        <h2 class="fw-bold h1 mb-1">Lorem ipsum</h2>
-        <p class="mb-0 text-muted text-end"><del>NT$1,200</del></p>
-        <p class="h4 fw-bold text-end">NT$1,080</p>
-        <div class="row align-items-center">
-          <div class="col-6">
-            <div class="input-group my-3 bg-light rounded">
-              <div class="input-group-prepend">
-                <button
-                  class="btn btn-outline-dark border-0 py-2"
-                  type="button"
-                  id="button-addon1"
-                >
-                  <font-awesome-icon :icon="['fas', 'minus']" />
-                </button>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img
+                  src="https://images.unsplash.com/photo-1502743780242-f10d2ce370f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1916&q=80"
+                  class="d-block w-100"
+                  alt="..."
+                />
               </div>
-              <input
-                type="text"
-                class="form-control border-0 text-center my-auto shadow-none bg-light"
-                placeholder=""
-                aria-label="Example text with button addon"
-                aria-describedby="button-addon1"
-                value="1"
-              />
-              <div class="input-group-append">
-                <button
-                  class="btn btn-outline-dark border-0 py-2"
-                  type="button"
-                  id="button-addon2"
-                >
-                  <font-awesome-icon :icon="['fas', 'plus']" />
-                </button>
+              <div class="carousel-item">
+                <img
+                  src="https://images.unsplash.com/photo-1502743780242-f10d2ce370f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1916&q=80"
+                  class="d-block w-100"
+                  alt="..."
+                />
+              </div>
+              <div class="carousel-item">
+                <img
+                  src="https://images.unsplash.com/photo-1502743780242-f10d2ce370f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1916&q=80"
+                  class="d-block w-100"
+                  alt="..."
+                />
               </div>
             </div>
-          </div>
-          <div class="col-6">
             <a
-              href="./checkout.html"
-              class="text-nowrap btn btn-dark w-100 py-2"
-              >Lorem ipsum</a
+              class="carousel-control-prev"
+              href="#carouselExampleControls"
+              role="button"
+              data-slide="prev"
             >
+              <span
+                class="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a
+              class="carousel-control-next"
+              href="#carouselExampleControls"
+              role="button"
+              data-slide="next"
+            >
+              <span
+                class="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+        </div>
+        <div class="col-md-5">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb bg-white px-0 mb-0 py-3">
+              <li class="breadcrumb-item">
+                <a class="text-muted" href="./index.html">Home</a>
+              </li>
+              <li class="breadcrumb-item">
+                <a class="text-muted" href="./product.html">Product</a>
+              </li>
+              <li class="breadcrumb-item active" aria-current="page">Detail</li>
+            </ol>
+          </nav>
+          <h2 class="fw-bold h1 mb-1">Lorem ipsum</h2>
+          <p class="mb-0 text-muted text-end"><del>NT$1,200</del></p>
+          <p class="h4 fw-bold text-end">NT$1,080</p>
+          <div class="row align-items-center">
+            <div class="col-6">
+              <div class="input-group my-3 bg-light rounded">
+                <div class="input-group-prepend">
+                  <button
+                    class="btn btn-outline-dark border-0 py-2"
+                    type="button"
+                    id="button-addon1"
+                  >
+                    <font-awesome-icon :icon="['fas', 'minus']" />
+                  </button>
+                </div>
+                <input
+                  type="text"
+                  class="form-control border-0 text-center my-auto shadow-none bg-light"
+                  placeholder=""
+                  aria-label="Example text with button addon"
+                  aria-describedby="button-addon1"
+                  value="1"
+                />
+                <div class="input-group-append">
+                  <button
+                    class="btn btn-outline-dark border-0 py-2"
+                    type="button"
+                    id="button-addon2"
+                  >
+                    <font-awesome-icon :icon="['fas', 'plus']" />
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div class="col-6">
+              <a
+                href="./checkout.html"
+                class="text-nowrap btn btn-dark w-100 py-2"
+                >Lorem ipsum</a
+              >
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="row my-5">
-      <div class="col-md-4">
-        <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua. At vero eos et accusam et
-        </p>
-      </div>
-      <div class="col-md-3">
-        <p class="text-muted">
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor
-        </p>
-      </div>
-    </div>
-    <h3 class="fw-bold">Lorem ipsum dolor sit amet</h3>
-    <SwiperSlider />
-  </div>
-  <div class="bg-light py-4">
-    <div class="container">
-      <div
-        class="d-flex flex-column flex-md-row justify-content-between align-items-md-center align-items-start"
-      >
-        <p class="mb-0 fw-bold">Lorem ipsum dolor sit amet.</p>
-        <div class="input-group w-md-50 mt-md-0 mt-3">
-          <input type="text" class="form-control rounded-0" placeholder="" />
-          <div class="input-group-append">
-            <button class="btn btn-dark rounded-0" type="button" id="search">
-              Lorem ipsum
-            </button>
-          </div>
+      <div class="row my-5">
+        <div class="col-md-4">
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et
+          </p>
+        </div>
+        <div class="col-md-3">
+          <p class="text-muted">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor
+          </p>
         </div>
       </div>
+      <h3 class="fw-bold">Lorem ipsum dolor sit amet</h3>
+      <SwiperSlider />
     </div>
-  </div>
-  <div class="bg-dark py-5">
-    <div class="container">
-      <div
-        class="d-flex align-items-center justify-content-between text-white mb-md-7 mb-4"
-      >
-        <a class="text-white h4" href="./index.html">LOGO</a>
-        <ul class="d-flex list-unstyled mb-0 h4">
-          <li>
-            <a href="#" class="text-white mx-3"
-              ><font-awesome-icon :icon="['fab', 'facebook']"
-            /></a>
-          </li>
-          <li>
-            <a href="#" class="text-white mx-3"
-              ><font-awesome-icon :icon="['fab', 'instagram']"
-            /></a>
-          </li>
-          <li>
-            <a href="#" class="text-white ms-3"
-              ><font-awesome-icon :icon="['fab', 'line']"
-            /></a>
-          </li>
-        </ul>
-      </div>
-      <div
-        class="d-flex flex-column flex-md-row justify-content-between align-items-md-end align-items-start text-white"
-      >
-        <div class="mb-md-0 mb-1">
-          <p class="mb-0">02-3456-7890</p>
-          <p class="mb-0">service@mail.com</p>
-        </div>
-        <p class="mb-0">© 2020 LOGO All Rights Reserved.</p>
-      </div>
-    </div>
-  </div>
+  </BaseLayout>
 </template>
 <style scoped></style>
