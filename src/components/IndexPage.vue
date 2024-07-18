1ffda5e8819eec7af1d932ed637f89bbe29e6fa2
misu13 <djmisu13@gmail.com>
2024-07-18 16:24:18 +0800

    fix:修正首頁重複程式碼拆入元件中

--- src/components/IndexPage.vue
+++ src/components/IndexPage.vue
@@ -15,34 +15,6 @@
       "
     ></div>
     <div class="container d-flex flex-column" style="min-height: 100vh">
-      <nav class="navbar navbar-expand-lg navbar-light">
-        <a class="navbar-brand" href="./index.html">Navbar</a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div
-          class="collapse navbar-collapse justify-content-end"
-          id="navbarNavAltMarkup"
-        >
-          <div class="navbar-nav">
-            <a class="nav-item nav-link me-4 active" href="./index.html"
-              >Home <span class="sr-only">(current)</span></a
-            >
-            <a class="nav-item nav-link me-4" href="./product.html">Product</a>
-            <a class="nav-item nav-link me-4" href="./detail.html">Detail</a>
-            <a class="nav-item nav-link" href="./cart.html"
-              ><font-awesome-icon :icon="['fas', 'shopping-cart']" /></a>
-          </div>
-        </div>
-      </nav>
       <div class="row justify-content-center my-auto">
         <div class="col-md-4 text-center">
           <h2>Lorem ipsum.</h2>
@@ -213,54 +185,6 @@
           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
           nonumy eirmod tempor invidunt ut labore et dolore magna.
         </p>
-      </div>
-    </div>
-  </div>
-  <div class="bg-light py-4">
-    <div class="container">
-      <div
-        class="d-flex flex-column flex-md-row justify-content-between align-items-md-center align-items-start"
-      >
-        <p class="mb-0 fw-bold">Lorem ipsum dolor sit amet.</p>
-        <div class="input-group w-md-50 mt-md-0 mt-3">
-          <input type="text" class="form-control rounded-0" placeholder="" />
-          <div class="input-group-append">
-            <button class="btn btn-dark rounded-0" type="button" id="search">
-              Lorem ipsum
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="bg-dark py-5">
-    <div class="container">
-      <div
-        class="d-flex align-items-center justify-content-between text-white mb-md-7 mb-4"
-      >
-        <a class="text-white h4" href="./index.html">LOGO</a>
-        <ul class="d-flex list-unstyled mb-0 h4">
-          <li>
-            <a href="#" class="text-white mx-3"
-              ><font-awesome-icon :icon="['fab', 'facebook']" /></a>
-          </li>
-          <li>
-            <a href="#" class="text-white mx-3"
-              ><font-awesome-icon :icon="['fab', 'instagram']" /></a>
-          </li>
-          <li>
-            <a href="#" class="text-white ms-3"><font-awesome-icon :icon="['fab', 'line']" /></a>
-          </li>
-        </ul>
-      </div>
-      <div
-        class="d-flex flex-column flex-md-row justify-content-between align-items-md-end align-items-start text-white"
-      >
-        <div class="mb-md-0 mb-1">
-          <p class="mb-0">02-3456-7890</p>
-          <p class="mb-0">service@mail.com</p>
-        </div>
-        <p class="mb-0">© 2020 LOGO All Rights Reserved.</p>
       </div>
     </div>
   </div>
--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,7 +1,58 @@
-<script setup>
-</script>
+<script setup></script>
 <template>
-    <div></div>
+  <div>
+    <div class="bg-light py-4">
+    <div class="container">
+      <div
+        class="d-flex flex-column flex-md-row justify-content-between align-items-md-center align-items-start"
+      >
+        <p class="mb-0 fw-bold">Lorem ipsum dolor sit amet.</p>
+        <div class="input-group w-md-50 mt-md-0 mt-3">
+          <input type="text" class="form-control rounded-0" placeholder="" />
+          <div class="input-group-append">
+            <button class="btn btn-dark rounded-0" type="button" id="search">
+              Lorem ipsum
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+    <div class="bg-dark py-5">
+      <div class="container">
+        <div
+          class="d-flex align-items-center justify-content-between text-white mb-md-7 mb-4"
+        >
+          <a class="text-white h4" href="./index.html">LOGO</a>
+          <ul class="d-flex list-unstyled mb-0 h4">
+            <li>
+              <a href="#" class="text-white mx-3"
+                ><font-awesome-icon :icon="['fab', 'facebook']"
+              /></a>
+            </li>
+            <li>
+              <a href="#" class="text-white mx-3"
+                ><font-awesome-icon :icon="['fab', 'instagram']"
+              /></a>
+            </li>
+            <li>
+              <a href="#" class="text-white ms-3"
+                ><font-awesome-icon :icon="['fab', 'line']"
+              /></a>
+            </li>
+          </ul>
+        </div>
+        <div
+          class="d-flex flex-column flex-md-row justify-content-between align-items-md-end align-items-start text-white"
+        >
+          <div class="mb-md-0 mb-1">
+            <p class="mb-0">02-3456-7890</p>
+            <p class="mb-0">service@mail.com</p>
+          </div>
+          <p class="mb-0">© 2020 LOGO All Rights Reserved.</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
-<style scoped>
-</style>
+<style scoped></style>
