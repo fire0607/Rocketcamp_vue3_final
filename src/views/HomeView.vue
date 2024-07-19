ab7cd30ad760be312c1fb779562014d5a050d70f
misu13 <djmisu13@gmail.com>
2024-07-19 16:35:54 +0800

    fix:修正404頁面路由錯誤

--- src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import IndexPage from './IndexPageView.vue'
-</script>
-
-<template>
-  <IndexPage />
-</template>
-
-<style scoped lang="scss"></style>
--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -5,7 +5,7 @@
             <div class="mt-7 justify-content-center">
                 <h2>找不到頁面！</h2>
             </div>
-            <router-link to="/Home" class="btn btn-dark rounded-0 mt-6 mb-6">回到首頁</router-link>
+            <router-link to="/" class="btn btn-dark rounded-0 mt-6 mb-6">回到首頁</router-link>
         </div>
         <div class="container"></div>
     </BaseLayout>
