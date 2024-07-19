5dc840c75c4ee1d9205905abbfc47cd6ff1939dc
misu13 <djmisu13@gmail.com>
2024-07-19 16:48:52 +0800

    fix:修正404頁面樣式

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -2,12 +2,13 @@
 <template>
     <BaseLayout :show-navbar="true" :show-footer="true" container-class="my-custom-container">
         <div class="container">
-            <div class="mt-7 justify-content-center">
-                <h2>找不到頁面！</h2>
+            <div class="row justify-content-center my-auto">
+                <div class="mt-7 col-md-4 text-center">
+                    <h2 class="fw-bolder">找不到頁面</h2>
+                    <router-link to="/" class="btn btn-dark rounded-0 mt-6 mb-6"><font-awesome-icon :icon="['fas', 'chevron-left']" class="me-2"/>回到首頁</router-link>
+                </div>
             </div>
-            <router-link to="/" class="btn btn-dark rounded-0 mt-6 mb-6">回到首頁</router-link>
         </div>
-        <div class="container"></div>
     </BaseLayout>
 </template>
 <style scoped>
