e24723fc24522a3a8c8efdc0ec211a2d315335a6
fire0607 <stu00634@gmail.com>
2024-07-22 23:40:31 +0800

    fix:修正footer樣式語法

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,16 +1,16 @@
 <script setup></script>
 <template>
   <div>
-    <div class="bg-light py-4">
+    <div class="bg-primary-1 py-4">
       <div class="container">
         <div
           class="d-flex flex-column flex-md-row justify-content-between align-items-md-center align-items-start"
         >
-          <p class="mb-0 fw-bold">留下您的mail，與我們聯繫</p>
+          <p class="mb-0 fw-bold text-primary-2">留下您的mail，與我們聯繫</p>
           <div class="input-group w-md-50 mt-md-0 mt-3">
-            <input type="text" class="form-control rounded-0" placeholder="" />
+            <input type="text" class="form-control rounded-0 custom-input" placeholder="" />
             <div class="input-group-append">
-              <button class="btn btn-dark rounded-0" type="button" id="search">
+              <button class="btn btn-primary-2 rounded-0" type="button" id="search">
                 送出
               </button>
             </div>
@@ -18,12 +18,12 @@
         </div>
       </div>
     </div>
-    <div class="bg-dark py-5">
+    <div class="bg-primary-2 py-5">
       <div class="container">
         <div
-          class="d-flex align-items-center justify-content-between text-white mb-md-7 mb-4"
+          class="d-flex align-items-center justify-content-between text-primary-1 mb-md-7 mb-4"
         >
-          <router-link to="/" class="text-white h4"
+          <router-link to="/" class="h4"
             ><h1>
               <img
                 src="/image/morning-dew-high-resolution-logo-transparent.png"
@@ -33,24 +33,24 @@
           ></router-link>
           <ul class="d-flex list-unstyled mb-0 h4">
             <li>
-              <a href="#" class="text-white mx-3"
+              <a href="#" class="text-primary-1 mx-3"
                 ><font-awesome-icon :icon="['fab', 'facebook']"
               /></a>
             </li>
             <li>
-              <a href="#" class="text-white mx-3"
+              <a href="#" class="text-primary-1 mx-3"
                 ><font-awesome-icon :icon="['fab', 'instagram']"
               /></a>
             </li>
             <li>
-              <a href="#" class="text-white ms-3"
+              <a href="#" class="text-primary-1 ms-3"
                 ><font-awesome-icon :icon="['fab', 'line']"
               /></a>
             </li>
           </ul>
         </div>
         <div
-          class="d-flex flex-column flex-md-row justify-content-between align-items-md-end align-items-start text-white"
+          class="d-flex flex-column flex-md-row justify-content-between align-items-md-end align-items-start text-primary-1"
         >
           <div class="mb-md-0 mb-1">
             <p class="mb-0">02-3456-7890</p>
@@ -63,19 +63,19 @@
   </div>
 </template>
 <style lang="scss" scoped>
-.bg-light {
-  background-color: #dce0c6 !important;
-  p {
-    color: #433c36 !important;
-  }
-  .btn {
-    background-color: #433c36 !important;
-  }
-}
-.bg-dark {
-  background-color: #433c36 !important;
-  p,a{
-    color: #dce0c6 !important;
-  }
-}
+// .bg-light {
+//   background-color: #dce0c6 !important;
+//   p {
+//     color: #433c36 !important;
+//   }
+//   .btn {
+//     background-color: #433c36 !important;
+//   }
+// }
+// .bg-dark {
+//   background-color: #433c36 !important;
+//   p,a{
+//     color: #dce0c6 !important;
+//   }
+// }
 </style>
