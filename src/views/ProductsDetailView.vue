676860992f4cbd601c266ad77373b4e7c9d88dc6
misu13 <djmisu13@gmail.com>
2024-07-22 14:29:29 +0800

    fix:修改產品資訊頁樣式

--- src/views/ProductsDetailView.vue
+++ src/views/ProductsDetailView.vue
@@ -19,65 +19,29 @@ import SwiperSlider from '../components/SwiperSlider.vue'
             <div class="carousel-inner">
               <div class="carousel-item active">
                 <img
-                  src="https://images.unsplash.com/photo-1502743780242-f10d2ce370f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1916&q=80"
-                  class="d-block w-100"
-                  alt="..."
-                />
-              </div>
-              <div class="carousel-item">
-                <img
-                  src="https://images.unsplash.com/photo-1502743780242-f10d2ce370f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1916&q=80"
-                  class="d-block w-100"
-                  alt="..."
-                />
-              </div>
-              <div class="carousel-item">
-                <img
-                  src="https://images.unsplash.com/photo-1502743780242-f10d2ce370f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1916&q=80"
+                  src="/image/product-1.png"
                   class="d-block w-100"
                   alt="..."
                 />
               </div>
             </div>
-            <a
-              class="carousel-control-prev"
-              href="#carouselExampleControls"
-              role="button"
-              data-slide="prev"
-            >
-              <span
-                class="carousel-control-prev-icon"
-                aria-hidden="true"
-              ></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a
-              class="carousel-control-next"
-              href="#carouselExampleControls"
-              role="button"
-              data-slide="next"
-            >
-              <span
-                class="carousel-control-next-icon"
-                aria-hidden="true"
-              ></span>
-              <span class="sr-only">Next</span>
-            </a>
           </div>
         </div>
         <div class="col-md-5">
           <nav aria-label="breadcrumb">
             <ol class="breadcrumb bg-white px-0 mb-0 py-3">
               <li class="breadcrumb-item">
-                <a class="text-muted" href="./index.html">Home</a>
+                <router-link to="/" class="text-muted"
+            >Home</router-link>
               </li>
               <li class="breadcrumb-item">
-                <a class="text-muted" href="./product.html">Product</a>
+                <router-link to="/products" class="text-muted"
+            >Product</router-link>
               </li>
               <li class="breadcrumb-item active" aria-current="page">Detail</li>
             </ol>
           </nav>
-          <h2 class="fw-bold h1 mb-1">Lorem ipsum</h2>
+          <h2 class="fw-bold h1 mb-1">晨曦微光</h2>
           <p class="mb-0 text-muted text-end"><del>NT$1,200</del></p>
           <p class="h4 fw-bold text-end">NT$1,080</p>
           <div class="row align-items-center">
@@ -115,7 +79,7 @@ import SwiperSlider from '../components/SwiperSlider.vue'
               <a
                 href="./checkout.html"
                 class="text-nowrap btn btn-dark w-100 py-2"
-                >Lorem ipsum</a
+                >加入購物車</a
               >
             </div>
           </div>
@@ -124,21 +88,56 @@ import SwiperSlider from '../components/SwiperSlider.vue'
       <div class="row my-5">
         <div class="col-md-4">
           <p>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et
+            捕捉清晨那一抹輕柔光芒，當橙花的清新香氣與檸檬草的淡雅芬芳交織在一起，仿佛在喚醒沉睡的大地。輕盈的白麝香如同晨曦中的薄霧，溫柔地包裹住一切。這支香水讓人彷彿置身於初升的陽光中，感受新一天的希望與能量。
           </p>
         </div>
         <div class="col-md-3">
-          <p class="text-muted">
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor
-          </p>
+          <p class="text-muted">香調： 橙花、檸檬草、輕盈的白麝香</p>
         </div>
       </div>
-      <h3 class="fw-bold">Lorem ipsum dolor sit amet</h3>
+      <h3 class="fw-bold">其他人也看了...</h3>
       <SwiperSlider />
     </div>
   </BaseLayout>
 </template>
-<style scoped></style>
+<style lang="scss" scoped>
+h2{
+  font-weight: 600 !important;
+  color: #433c36;
+  word-break: break-word;
+}
+h4{
+  font-weight: 600 !important;
+  color: #433c36;
+  word-break: break-word;
+}
+p{
+  font-weight: 500 !important;
+  line-height: 1.8;
+  color: #433c36 !important;
+  word-break: break-word;
+}
+.btn-dark{
+  background-color: #d8c062;
+  border: #d8c062;
+  &:hover {
+      background-color: rgba(216, 192, 98,0.7);
+      transition: .3s;
+    }
+}
+.input-group{
+  background-color: #dce0c6 !important;
+  input{
+    background-color: rgb(220, 224, 198) !important;
+    color:  #433c36;
+    font-weight: 600;
+  }
+}
+#button-addon1,#button-addon2{
+  color:  #433c36 !important;
+  &:hover{
+    background-color: rgb(201, 203, 164);
+    transition: .3s;
+  }
+}
+</style>
