6ed7c33ba7df38b8ca8fec30aa54aea6c6758be6
fire0607 <stu00634@gmail.com>
2024-07-23 00:07:22 +0800

    fix:修正首頁樣式語法

--- src/views/IndexPageView.vue
+++ src/views/IndexPageView.vue
@@ -21,9 +21,9 @@ import TheNavbar from '../components/TheNavbar.vue'
     <div class="container d-flex flex-column" style="min-height: 100vh">
       <TheNavbar />
       <div class="row justify-content-center my-auto">
-        <div class="col-md-4 text-center">
-          <h2>清晨微光 Morning Dew</h2>
-          <p class="text-muted mb-0">
+        <div class="col-md-4 text-center text-primary-2">
+          <h2 class="fw-bolder">清晨微光<br>Morning Dew</h2>
+          <p class="text-primary-2 mb-0 lh-base lh-lg">
             清晨微光的每一瓶香水，都像是一個迷人的清晨，<br>帶著自然的清新和希望。
           </p>
           <router-link to="/products" class="btn btn-dark rounded-0 mt-4">了解更多</router-link>
@@ -31,19 +31,19 @@ import TheNavbar from '../components/TheNavbar.vue'
       </div>
     </div>
   </div>
-  <div class="container">
+  <div class="container lh-lg">
     <div class="row mt-5">
-      <div class="col-md-4 mt-md-4">
+      <div class="col-md-4 mt-md-4 ">
         <div class="card border-0 mb-4">
           <img
             src="/image/product-1.png"
             class="card-img-top rounded-0"
             alt="..."
           />
-          <div class="card-body text-center">
-            <h4>清晨鳥啼</h4>
+          <div class="card-body text-center text-primary-2">
+            <h4 class="fw-bolder">清晨鳥啼</h4>
             <div class="d-flex justify-content-between">
-              <p class="card-text text-muted mb-0">
+              <p class="card-text text-primary-2 mb-0 text-center">
                 早春的清新薄荷、花梨木、晨露滴落的綠茶葉
               </p>
             </div>
@@ -57,10 +57,10 @@ import TheNavbar from '../components/TheNavbar.vue'
             class="card-img-top rounded-0"
             alt="..."
           />
-          <div class="card-body text-center">
-            <h4>初曉花園</h4>
+          <div class="card-body text-center text-primary-2">
+            <h4 class="fw-bolder">初曉花園</h4>
             <div class="d-flex justify-content-between">
-              <p class="card-text text-muted mb-0">
+              <p class="card-text text-primary-2 mb-0 text-center">
                 清晨的玫瑰花瓣、繡球花、些許的青草香
               </p>
             </div>
@@ -74,10 +74,10 @@ import TheNavbar from '../components/TheNavbar.vue'
             class="card-img-top rounded-0"
             alt="..."
           />
-          <div class="card-body text-center">
-            <h4>薄霧森林</h4>
+          <div class="card-body text-center text-primary-2">
+            <h4 class="fw-bolder">薄霧森林</h4>
             <div class="d-flex justify-content-between">
-              <p class="card-text text-muted mb-0">
+              <p class="card-text text-primary-2 mb-0 text-center">
                 杉木、柏樹、輕霧中的白蘭地
               </p>
             </div>
@@ -86,7 +86,7 @@ import TheNavbar from '../components/TheNavbar.vue'
       </div>
     </div>
   </div>
-  <div class="bg-light mt-7">
+  <div class="bg-primary-1 mt-7 lh-lg">
     <div class="container">
       <div
         id="carouselExampleControls"
@@ -96,9 +96,9 @@ import TheNavbar from '../components/TheNavbar.vue'
         <div class="carousel-inner">
           <div class="carousel-item active">
             <div class="row justify-content-center py-7">
-              <div class="col-md-6 text-center">
-                <h3>微光晨曦，綻放自然之美</h3>
-                <p class="my-5">
+              <div class="col-md-6 text-center text-primary-2 mt-5 mb-3">
+                <h3 class="fw-bolder">微光晨曦，綻放自然之美</h3>
+                <p class="my-5 text-primary-2">
                   品牌專注於使用純淨的自然材料，<br>為都會女性創造一種純粹而輕盈的香氣，<br>讓您在繁忙的生活中找到一絲寧靜與舒適。
                 </p>
                 <p><small>— Dawn's gentle light, unveiling the beauty of nature. —</small></p>
@@ -109,7 +109,7 @@ import TheNavbar from '../components/TheNavbar.vue'
       </div>
     </div>
   </div>
-  <div class="container my-7">
+  <div class="container my-7 lh-lg mt-5 mb-5">
     <div class="row">
       <div class="col-md-6">
         <img
@@ -119,9 +119,9 @@ import TheNavbar from '../components/TheNavbar.vue'
         />
       </div>
       <div class="col-md-4 m-auto text-center">
-        <h4 class="mt-4">晨曦微光</h4>
-        <p class="text-muted">
-          捕捉清晨那一抹輕柔光芒，<br>當橙花的清新香氣與檸檬草的淡雅芬芳交織在一起，<br>仿佛在喚醒沉睡的大地。<br>輕盈的白麝香如同晨曦中的薄霧，<br>溫柔地包裹住一切。<br>這支香水讓人彷彿置身於初升的陽光中，<br>感受新一天的希望與能量。
+        <h4 class="mt-4 text-primary-2 fw-bolder">晨曦微光</h4>
+        <p class="text-primary-2">
+          捕捉清晨那一抹輕柔光芒，<br>當橙花的清新香氣與檸檬草的淡雅芬芳交織，<br>仿佛在喚醒沉睡的大地。<br>輕盈的白麝香如同晨曦中的薄霧，<br>溫柔地包裹住一切。<br>這支香水讓人彷彿置身於初升的陽光中，<br>感受新一天的希望與能量。
         </p>
       </div>
     </div>
@@ -134,9 +134,9 @@ import TheNavbar from '../components/TheNavbar.vue'
         />
       </div>
       <div class="col-md-4 m-auto text-center">
-        <h4 class="mt-4">晨風綠意</h4>
-        <p class="text-muted">
-          將清晨的清新氣息封存於瓶中。<br>綠茶的淡雅與百里香的清爽相互輝映，<br>帶來一絲綠意盎然的感覺，<br>早晨雨後松木的淡淡香氣彷彿喚起在森林中散步的愜意。<br>這支香水讓人如同置身於清晨的森林中，<br>呼吸著雨後的清新空氣，<br>享受片刻的平靜與悠然。
+        <h4 class="mt-4 text-primary-2 fw-bolder">晨風綠意</h4>
+        <p class="text-primary-2">
+          將清晨的清新氣息封存於瓶中。<br>綠茶的淡雅與百里香的清爽相互輝映，<br>帶來一絲綠意盎然的感覺，<br>雨後松木的香氣彷彿喚起在森林中散步的愜意。<br>這支香水讓人如同置身於清晨的森林中，<br>呼吸著雨後的清新空氣，<br>享受片刻的平靜與悠然。
         </p>
       </div>
     </div>
@@ -144,45 +144,4 @@ import TheNavbar from '../components/TheNavbar.vue'
   </BaseLayout>
 </template>
 
-<style lang="scss" scoped>
-h2{
-  font-weight: 600 !important;
-  color: #433c36;
-  word-break: break-word;
-}
-h3{
-  font-weight: 600 !important;
-  color: #433c36;
-  word-break: break-word;
-}
-h4{
-  font-weight: 600 !important;
-  color: #433c36;
-  word-break: break-word;
-}
-p{
-  font-weight: 500 !important;
-  line-height: 1.8;
-  color: #433c36 !important;
-  word-break: break-word;
-}
-.btn{
-  background-color: #d8c062;
-  border: #d8c062;
-}
-.card-body {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.card-text {
-  width: 100%;
-  text-align: center;
-}
-.bg-light{
-  background-color: #dce0c6 !important;
-}
-
-</style>
+<style scoped></style>
