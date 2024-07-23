6096e1e0567e1d590a536b9554a08e7aefa76471
misu13 <djmisu13@gmail.com>
2024-07-23 10:27:52 +0800

    fix:修正產品詳情頁樣式語法

--- src/views/ProductsDetailView.vue
+++ src/views/ProductsDetailView.vue
@@ -8,7 +8,7 @@ import SwiperSlider from '../components/SwiperSlider.vue'
     :show-footer="true"
     container-class="my-custom-container"
   >
-    <div class="container">
+    <div class="container text-primary-2 lh-lg">
       <div class="row align-items-center">
         <div class="col-md-7">
           <div
@@ -31,25 +31,25 @@ import SwiperSlider from '../components/SwiperSlider.vue'
           <nav aria-label="breadcrumb">
             <ol class="breadcrumb bg-white px-0 mb-0 py-3">
               <li class="breadcrumb-item">
-                <router-link to="/" class="text-muted"
+                <router-link to="/" class="text-primary-2"
             >Home</router-link>
               </li>
               <li class="breadcrumb-item">
-                <router-link to="/products" class="text-muted"
+                <router-link to="/products" class="text-primary-2"
             >Product</router-link>
               </li>
               <li class="breadcrumb-item active" aria-current="page">Detail</li>
             </ol>
           </nav>
-          <h2 class="fw-bold h1 mb-1">晨曦微光</h2>
-          <p class="mb-0 text-muted text-end"><del>NT$1,200</del></p>
+          <h2 class="fw-bold h1 mb-1 text-primary-2">晨曦微光</h2>
+          <p class="mb-0 text-primary-2 text-end"><del>NT$1,200</del></p>
           <p class="h4 fw-bold text-end">NT$1,080</p>
           <div class="row align-items-center">
             <div class="col-6">
-              <div class="input-group my-3 bg-light rounded">
+              <div class="input-group my-3 bg-primary-1 rounded text-primary-2">
                 <div class="input-group-prepend">
                   <button
-                    class="btn btn-outline-dark border-0 py-2"
+                    class="btn btn-outline-dark border-0 py-2 bg-primary-1"
                     type="button"
                     id="button-addon1"
                   >
@@ -58,7 +58,7 @@ import SwiperSlider from '../components/SwiperSlider.vue'
                 </div>
                 <input
                   type="text"
-                  class="form-control border-0 text-center my-auto shadow-none bg-light"
+                  class="form-control border-0 text-center my-auto shadow-none bg-primary-1"
                   placeholder=""
                   aria-label="Example text with button addon"
                   aria-describedby="button-addon1"
@@ -66,7 +66,7 @@ import SwiperSlider from '../components/SwiperSlider.vue'
                 />
                 <div class="input-group-append">
                   <button
-                    class="btn btn-outline-dark border-0 py-2"
+                    class="btn btn-outline-dark border-0 py-2 bg-primary-1"
                     type="button"
                     id="button-addon2"
                   >
@@ -78,7 +78,7 @@ import SwiperSlider from '../components/SwiperSlider.vue'
             <div class="col-6">
               <a
                 href="./checkout.html"
-                class="text-nowrap btn btn-dark w-100 py-2"
+                class="text-nowrap btn btn-primary-0 w-100 py-2"
                 >加入購物車</a
               >
             </div>
@@ -92,7 +92,7 @@ import SwiperSlider from '../components/SwiperSlider.vue'
           </p>
         </div>
         <div class="col-md-3">
-          <p class="text-muted">香調： 橙花、檸檬草、輕盈的白麝香</p>
+          <p class="text-primary-2 fw-bolder">香調： 橙花、檸檬草、輕盈的白麝香</p>
         </div>
       </div>
       <h3 class="fw-bold">其他人也看了...</h3>
@@ -100,44 +100,4 @@ import SwiperSlider from '../components/SwiperSlider.vue'
     </div>
   </BaseLayout>
 </template>
-<style lang="scss" scoped>
-h2{
-  font-weight: 600 !important;
-  color: #433c36;
-  word-break: break-word;
-}
-h4{
-  font-weight: 600 !important;
-  color: #433c36;
-  word-break: break-word;
-}
-p{
-  font-weight: 500 !important;
-  line-height: 1.8;
-  color: #433c36 !important;
-  word-break: break-word;
-}
-.btn-dark{
-  background-color: #d8c062;
-  border: #d8c062;
-  &:hover {
-      background-color: rgba(216, 192, 98,0.7);
-      transition: .3s;
-    }
-}
-.input-group{
-  background-color: #dce0c6 !important;
-  input{
-    background-color: rgb(220, 224, 198) !important;
-    color:  #433c36;
-    font-weight: 600;
-  }
-}
-#button-addon1,#button-addon2{
-  color:  #433c36 !important;
-  &:hover{
-    background-color: rgb(201, 203, 164);
-    transition: .3s;
-  }
-}
-</style>
+<style scoped></style>
