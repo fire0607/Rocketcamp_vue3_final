76f2620323838cb66313e70f9c5c761a3230d2de
misu13 <djmisu13@gmail.com>
2024-07-29 14:16:59 +0800

    feat:新增購物車基本邏輯

--- src/views/ShoppingCartView.vue
+++ src/views/ShoppingCartView.vue
@@ -1,5 +1,17 @@
 <script setup>
+import { useCartStore } from '@/stores/cartStore'
 import SwiperSlider from '../components/SwiperSlider.vue'
+
+const cartStore = useCartStore()
+
+const removeItem = (productId) => {
+  cartStore.removeFromCart(productId)
+}
+
+const updateQuantity = (productId, newQuantity) => {
+  cartStore.updateQuantity(productId, newQuantity)
+}
+
 </script>
 <template>
   <BaseLayout
@@ -22,96 +34,31 @@ import SwiperSlider from '../components/SwiperSlider.vue'
                 </tr>
               </thead>
               <tbody>
-                <tr class="border-bottom border-top">
+                <tr v-for="item in cartStore.items" :key="item.id" class="border-bottom border-top">
                   <th scope="row" class="border-0 px-0 font-weight-normal py-4 text-primary-2">
-                    <img
-                      src="https://images.unsplash.com/photo-1502743780242-f10d2ce370f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1916&q=80"
-                      alt=""
-                      style="width: 72px; height: 72px; object-fit: cover"
-                    />
-                    <p class="mb-0 fw-bold ms-3 d-inline-block">Lorem ipsum</p>
+                    <img :src="item.imageUrl" alt="" style="width: 72px; height: 72px; object-fit: cover" />
+                    <p class="mb-0 fw-bold ms-3 d-inline-block">{{ item.title }}</p>
                   </th>
                   <td class="border-0 align-middle" style="max-width: 160px">
                     <div class="input-group pe-5">
                       <div class="input-group-prepend">
-                        <button
-                          class="btn btn-white-primary-0 border-0 py-2"
-                          type="button"
-                          id="button-addon1"
-                        >
+                        <button @click="updateQuantity(item.id, item.quantity - 1)" class="btn btn-white-primary-0 border-0 py-2" type="button">
                           <font-awesome-icon :icon="['fas', 'minus']" />
                         </button>
                       </div>
-                      <input
-                        type="text"
-                        class="form-control border-0 text-center my-auto shadow-none"
-                        placeholder=""
-                        aria-label="Example text with button addon"
-                        aria-describedby="button-addon1"
-                        value="1"
-                      />
+                      <input type="text" class="form-control border-0 text-center my-auto shadow-none" v-model="item.quantity" />
                       <div class="input-group-append">
-                        <button
-                          class="btn btn-white-primary-0 border-0 py-2"
-                          type="button"
-                          id="button-addon2"
-                        >
+                        <button @click="updateQuantity(item.id, item.quantity + 1)" class="btn btn-white-primary-0 border-0 py-2" type="button">
                           <font-awesome-icon :icon="['fas', 'plus']" />
                         </button>
                       </div>
                     </div>
                   </td>
                   <td class="border-0 align-middle">
-                    <p class="mb-0 ms-auto text-primary-2">NT$12,000</p>
+                    <p class="mb-0 ms-auto text-primary-2">NT${{ item.price * item.quantity }}</p>
                   </td>
                   <td class="border-0 align-middle">
-                    <font-awesome-icon :icon="['fas', 'times']" class="btn btn-white-primary-0"/>
-                  </td>
-                </tr>
-                <tr class="border-bottom">
-                  <th scope="row" class="border-0 px-0 font-weight-normal py-4">
-                    <img
-                      src="https://images.unsplash.com/photo-1502743780242-f10d2ce370f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1916&q=80"
-                      alt=""
-                      style="width: 72px; height: 72px; object-fit: cover"
-                    />
-                    <p class="mb-0 fw-bold ms-3 d-inline-block">Lorem ipsum</p>
-                  </th>
-                  <td class="border-0 align-middle" style="max-width: 160px">
-                    <div class="input-group pe-5">
-                      <div class="input-group-prepend">
-                        <button
-                          class="btn btn-white-primary-0 border-0 py-2"
-                          type="button"
-                          id="button-addon1"
-                        >
-                          <font-awesome-icon :icon="['fas', 'minus']" />
-                        </button>
-                      </div>
-                      <input
-                        type="text"
-                        class="form-control border-0 text-center my-auto shadow-none"
-                        placeholder=""
-                        aria-label="Example text with button addon"
-                        aria-describedby="button-addon1"
-                        value="1"
-                      />
-                      <div class="input-group-append">
-                        <button
-                          class="btn btn-white-primary-0 border-0 py-2"
-                          type="button"
-                          id="button-addon2"
-                        >
-                          <font-awesome-icon :icon="['fas', 'plus']" />
-                        </button>
-                      </div>
-                    </div>
-                  </td>
-                  <td class="border-0 align-middle">
-                    <p class="mb-0 ms-auto">NT$12,000</p>
-                  </td>
-                  <td class="border-0 align-middle">
-                    <font-awesome-icon :icon="['fas', 'times']" class="btn btn-white-primary-0"/>
+                    <font-awesome-icon @click="removeItem(item.id)" :icon="['fas', 'times']" class="btn btn-white-primary-0"/>
                   </td>
                 </tr>
               </tbody>
@@ -141,10 +88,8 @@ import SwiperSlider from '../components/SwiperSlider.vue'
               <table class="table text-muted border-bottom">
                 <tbody>
                   <tr>
-                    <th scope="row" class="border-0 px-0 pt-4 font-weight-normal text-primary-2">
-                      小計
-                    </th>
-                    <td class="text-end border-0 px-0 pt-4 text-primary-2">NT$24,000</td>
+                    <th scope="row" class="border-0 px-0 pt-4 font-weight-normal text-primary-2">小計</th>
+                    <td class="text-end border-0 px-0 pt-4 text-primary-2">NT${{ cartStore.totalAmount }}</td>
                   </tr>
                   <tr>
                     <th
@@ -159,7 +104,7 @@ import SwiperSlider from '../components/SwiperSlider.vue'
               </table>
               <div class="d-flex justify-content-between mt-4">
                 <p class="mb-0 h4 fw-bold">合計</p>
-                <p class="mb-0 h4 fw-bold">NT$24,000</p>
+                <p class="mb-0 h4 fw-bold">NT${{ cartStore.totalAmount }}</p>
               </div>
               <router-link to="/checkout" class="btn btn-primary-2 w-100 mt-4">送出訂單</router-link>
             </div>
