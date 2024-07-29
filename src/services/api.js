dfdfbb1484636ce533d0b66cba475202e2e96b01
fire0607 <stu00634@gmail.com>
2024-07-30 00:41:34 +0800

    fix:移除頁面登入登出功能

--- src/services/api.js
+++ src/services/api.js
@@ -3,31 +3,12 @@ import axios from 'axios'
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_APP_API_URL,
   headers: {
-    'Content-Type': 'application/json',
-    Authorization: localStorage.getItem('token')
-  },
-  withCredentials: true
-})
-
-apiClient.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token')
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`
-    }
-    return config
-  },
-  (error) => {
-    return Promise.reject(error)
+    'Content-Type': 'application/json'
   }
-)
+})
 
 export const getProducts = () => apiClient.get(`/api/${import.meta.env.VITE_APP_API_NAME}/products/all`)
 export const getProduct = (id) => apiClient.get(`/api/${import.meta.env.VITE_APP_API_NAME}/product/${id}`)
-
-export const login = (username, password) => apiClient.post('/admin/signin', { username, password })
-export const logout = () => apiClient.post('/logout')
-export const checkLoginStatus = () => apiClient.post('/api/user/check')
 
 export const cartAPI = {
   addToCart: (data) => apiClient.post(`/api/${import.meta.env.VITE_APP_API_NAME}/cart`, { data }),
